5e6030a620d9ef78dec6b867ece0956eba382a5c
Ing-GaryDiaz <ing.garydiaz@gmail.com>
2025-04-25 20:27:50 -0500

    Commit_1

---
+++ src/main/java/Example.java
@@ -0,0 +1,11 @@
+public class Example {
+    public int isPositive(int n) {
+        if (n == 0) {
+            return -1;
+        }
+        if (n > 0) {
+            return 1;
+        }
+        return 0;
+    }
+}
